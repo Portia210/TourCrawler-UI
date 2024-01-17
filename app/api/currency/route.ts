73572bcd9eea35b0808779bf7b9d1d7535a37ca1
khoavn02 <khoavungoc02@gmail.com>
2024-01-17 15:30:03 +0700

    update currency api

--- app/api/currency/route.ts
+++ app/api/currency/route.ts
@@ -9,7 +9,14 @@ import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   try {
     await connectMongoDB();
-    const currencies = await currencyService.getCurrencies();
+    let currencies = await currencyService.getCurrencies();
+    const baseCurrency = {
+      currency: currencies.base,
+      name: "United States Dollar",
+      rate: 1,
+    };
+    currencies.rates.push(baseCurrency);
+    currencies.rates = currencies.rates.reverse();
     return nextReturn(currencies, 200, "OK");
   } catch (err: any) {
     return nextReturn(err?.message || err, 500, "INTERNAL_SERVER_ERROR");
