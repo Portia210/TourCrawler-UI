e18e7c67dae7aec4c93e9ee195046d9c6457390e
khoavn02 <khoavungoc02@gmail.com>
2024-01-23 14:35:47 +0700

    optimzie travelor sync

--- app/api/travelor/sync/route.ts
+++ app/api/travelor/sync/route.ts
@@ -10,8 +10,20 @@ export async function POST(request: NextRequest) {
   try {
     await connectMongoDB();
     const hotels = await request.json();
-    const travelorHotels = hotels.map((hotel: any) => new TravelorHotel(hotel));
-    await TravelorHotel.insertMany(travelorHotels);
+    const bulkOps = hotels.map((hotel: any) => ({
+      updateOne: {
+        filter: {
+          travelor_link: hotel?.travelor_link,
+        },
+        update: {
+          $set: {
+            ...hotel,
+          },
+        },
+        upsert: true,
+      },
+    }));
+    await TravelorHotel.bulkWrite(bulkOps);
     return nextReturn("Synced successfully", 200, "OK");
   } catch (err: any) {
     return nextReturn(err?.message || err, 500, "INTERNAL_SERVER_ERROR");
